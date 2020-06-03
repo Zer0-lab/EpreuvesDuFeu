4c99a928e522d5e95a17eaa52818685664b73210
Zer0-lab <kevin.dhoust@gmail.com>
2020-06-03 09:58:16 +0200

    Add anagram

---
+++ anagram/anagram.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+
+wordList = list(open("fr.txt"))
+# words = str(sys.argv[1::])
+anagram = "arbre"
+word2 = ""
+
+def removeSpace(list):
+    newList =[]
+    for i in list:
+        newList.append(i.strip())
+    return newList
+
+def toSepare(word):
+    word2 = ""
+    for i in sorted(word):
+        word2 += ''.join(i)
+    return word2
+
+wordList = removeSpace(wordList)
+
+i = 0
+while i < len(wordList):
+    if toSepare(wordList[i]) == toSepare(anagram):
+        if(wordList[i] != anagram):
+            print("L'anagrame de %s est %s " % (anagram, wordList[i]))
+    i += 1
