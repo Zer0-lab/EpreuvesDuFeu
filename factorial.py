96bedc36cf5d9b3b88b57cccf98aff61fcc8c502
Zer0-lab <kevin.dhoust@gmail.com>
2020-06-07 17:53:20 +0200

    correction factorial

--- factorial.py
+++ factorial.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-number = int(sys.argv[1])
+number = 42
+
+#int(sys.argv[1])
 
 def factorial(nb):
     nbRange = range(nb)
     for i in nbRange[::-1]:
         if i >= 1:
             nb = nb*i
-            print(nb, i)
-        else:
             return nb
 
 
