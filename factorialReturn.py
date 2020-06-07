e7d5791df1da0943c49443df78ed9b66b31d169b
Zer0-lab <kevin.dhoust@gmail.com>
2020-06-07 17:43:26 +0200

    Add factorialReturn.py

---
+++ factorialReturn.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+number = 42
+
+#int(sys.argv[1])
+
+def factorial(nb):
+    nbRange = range(nb)
+    for i in nbRange[::-1]:
+        if i >= 1:
+            nb = nb*i
+        else:
+            return nb
+
+
+print("Le factorielle de %s est %s " % (number, factorial(number)))
